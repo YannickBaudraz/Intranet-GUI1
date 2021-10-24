d7e4f1ece37d9f25757e8913780bce74f0ea8783
Yannick Baudraz <Yannick.BAUDRAZ@cpnv.ch>
2021-10-25 00:51:58 +0200

    Add menu slider

--- templates/components/menuCalendar.php
+++ templates/components/menuCalendar.php
@@ -3,18 +3,30 @@
 ob_start();
 ?>
     <div id="menu">
-        <div class="menuBurger">
-            <i class="fas fa-bars fa-2x"></i>
-        </div>
-
+        <div></div>
         <div class="menuWeek">
-
             <i class="fas fa-arrow-circle-left"></i>
             <div><span>6 septembre 2021</span></div>
             <i class="fas fa-arrow-circle-right"></i>
-
         </div>
         <div class="week-dayButton"><i class="fas fa-calendar-week"></i></div>
+    </div>
+    <div id="menu-container">
+        <div class="bg"></div>
+        <div class="burger" tabindex="0">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </div>
+        <div id="nav-content" tabindex="0">
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Agenda</a></li>
+                <li><a href="#">Classes</a></li>
+                <li><a href="#">Cours</a></li>
+                <li><a href="#">Liste</a></li>
+            </ul>
+        </div>
     </div>
     <div class="after-menu"></div>
 <?php
